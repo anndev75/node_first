6267a8a9662fbaad295216c85c350194192d4ec3
Angel Guevara <167249763+anndev75@users.noreply.github.com>
2025-01-22 12:01:19 -0500

    Para Github v2

--- index.js
+++ index.js
@@ -1,80 +1,52 @@
 const readline = require('readline');
-const interface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const main_interface = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
 });
 
-function leer(mensaje) {
+// interfaz global
+const scan = (msj)=>{
     return new Promise((resolve) => {
-        interface.question(mensaje, respuesta => resolve(respuesta));
+        main_interface.question(msj, resolve);
+    })
+};
+
+
+//modulos necesarios
+const fs = require('fs');
+const path = require('path');
+const dir_ejercicios = path.join(__dirname, 'ejercicios');
+const list_archivos = fs.readdirSync(dir_ejercicios).filter(archivo => archivo.endsWith('.js')); //all js
+
+function imprimirMenu(){
+    console.log("EJERCICIOS Y PROGRAMAS JS");
+    list_archivos.forEach((archivo, numero)=>{
+        console.log((numero+1)+". "+archivo);
     });
-}
-
-async function clasificar(list, type) {
-    let clasificado;
-    switch (type) {
-        case 1:
-            const userid = await leer('ID: ');
-            clasificado = list.filter(({ id }) => id === parseInt(userid));
-            break;
-        case 2:
-            const nombre = await leer('Nombre: ');
-            clasificado = list.filter(({ name }) => 
-                name.toLowerCase().includes(nombre.toLowerCase())
-            );
-            break;
-        case 3:
-            const rol = await leer('Rol: ');
-            clasificado = list.filter(({ role }) => 
-                role.toLowerCase().includes(rol.toLowerCase())
-            );
-            break;
-        case 4:
-            clasificado = list;
-            break;
-        case 5:
-            clasificado = "Adiós, buenas noches :VV";
-            break;
-        default:
-            clasificado = "Opción no válida";
-            break;
-    }
-    return clasificado;
-}
-
-function imprimirMenu() {
-    console.log("MENU MISTERIOSO KLK");
-    console.log("1. Buscar por ID");
-    console.log("2. Buscar por Nombre");
-    console.log("3. Buscar por Rol");
-    console.log("4. Mostrar todos");
-    console.log("5. Salir\n");
+    console.log("0. Salir\n")
 }
 
 (async function main() {
+    console.clear();
     let menu;
     do {
         imprimirMenu();
-        menu = parseInt(await leer('Escoge una opción: '));
-        if (menu === 5) {
-            console.log("Adiós, buenas noches :VV");
-            break;
-        }
+        menu = await scan("¿A qué archivo quieres acceder? (n°): ");
+        console.clear();
+        //importamos el modulo
+        if(menu != 0){
+            try {
+                const ruta = path.join(dir_ejercicios,list_archivos[menu-1]);
+                const modulo = require(ruta);
+                console.log("Accediento a "+list_archivos[menu-1]+"\n");
+                await modulo(scan)//pasamos la interfaz única stdin / stdout
 
-        if (![1, 2, 3, 4].includes(menu)) {
-            console.log("Opción no válida\n");
-            continue;
+            } catch (error) {
+                console.log(error.message)
+                console.error("Error en el archivo \n");
+            }72+5
         }
-
-        try {
-            const resolve = await fetch('https://api.escuelajs.co/api/v1/users');
-            if (!resolve.ok) throw new Error(`Error en la API: ${resolve.status}`);
-            const data = await resolve.json();
-            const resultado = await clasificar(data, menu);
-            console.log(resultado);
-        } catch (error) {
-            console.error('Error al obtener datos:', error.message);
-        }
-    } while (menu !== 5);
-    interface.close();
+        
+    } while (menu != 0);
+    main_interface.close();
 })();
